4eec63bf61ba6fac88e455e923fd4e33cade4844
Ivan Mamenemuno <ivan.m@ims-evolve.com>
2019-10-23 11:12:08 +0100

    Exodas ims mac

---
+++ src/components/card/card.component.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import './card.styles.css';
+export const Card = ({ monster }) => {
+  return <div className="card-container">
+    <img src={`https://robohash.org/${monster.id}?set=set2&size=180x240`} alt="monster"/>
+    <h2>{monster.name}</h2>
+    <p> {monster.email} </p>
+  </div>
+}
